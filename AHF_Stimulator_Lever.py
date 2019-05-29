9c973aa6d6679a67a29079ccdba7286d46e7ac36
Jamie Boyd <jadobo@gmail.com>
2019-05-29 16:17:26 -0700

    configuration for default

--- AHF_Stimulator_Lever.py
+++ AHF_Stimulator_Lever.py
@@ -15,15 +15,16 @@ class AHF_Stimulator_Lever (AHF_Stimulator):
     AHF_Stimulator_Lever runs a lever task by calling ptLeverThread C-module code that records lever position and puts torque on the lever
     """
     LEVER_FREQ = 250 # as set in leverThread.h when ptLeverThread c module is compiled. Frequency in Hz of updating lever position and lever force
+    MAX_FORCE_ARRAY_SIZE = 125 # as set in leverThread.h when ptLeverThread c module is compiled. Maximum size of array for calculating perturb force ramp
     """
-    Global settings for the task, things that do not vary on a per mouse basis
-    These inlcude hardware settings, trial timing, 
+    ######################################### Global settings for the task########################################################
+    Things that do not vary on a per mouse basis, these inlcude hardware settings, some trial timing
     """
     defaultRecordingTime = 4 #  maximum of seconds of lever position data that will be recorded per trial
     defaultLeverIsReversed = False # False means lever position numbers start at 0 and get bigger when mouse pulls lever, True means numbers get smaller
     defaultGoalCuePin = 23 # GPIO pin to use for a cue played when lever is in goal pos
     defaultGoalCueFreq = 0 # frequency for goal cue - 0 means DC, i.e., turn ON and OFF
-    defaultConstForce = 1300 # constant force on lever at stgart of trial that mouse must pull against, as opposed to perturbForce set during a trial a 12 bit value, 0 to 4095
+    defaultConstForce = 1300 # constant force on lever at start of trial that mouse must pull against, as opposed to perturbForce set during a trial a 12 bit value, 0 to 4095
     defaultTrialIsCued = False # True means trials will be started with the start cue; False means mouse starts pulling lever whenever he likes
     """
     Settings for cued trials
@@ -31,15 +32,51 @@ class AHF_Stimulator_Lever (AHF_Stimulator):
     defaultStartCuePin = 27 # GPIO pin to use for a cue when mouse should start a trial, this can be a pulse (freq =0) or a train
     defaultStartCueFreq = 0 # frequency for start cue - 0 means DC, i.e., turn ON and OFF
     defaultStartCueDur = 0.1 # duration in seconds of start cue
-    defaultToGoalTime = .25 # mouse has this many seconds from start cue to get lever into goal area, or trial is ended. toGoalTime plus holdTime must be < recordingTime
     defaultTrialTimeout = 0.5 # time in seconds between end of one trial (ended for any reason) and start of the next trial. Lever is rezeroed here
     """
     Settings for uncued trials
     """
-    defaultPrePullTime = 0.25 # this many seconds of time before the lever reaches goal position is saved in a circular buffer 
-
-
-    #pathToData_def = '/home/pi/Documents/' # folder where lever pos files will be saved
+    defaultPrePullTime = 0.25 # this many seconds of time before the lever reaches goal position is saved in a circular buffer
+    """
+    ############################################# Mouse Specific Settings follow #####################################################
+    default pull difficulty settings for starting a mouse
+    toGoalTime plus holdTime must be < recordingTime for cued trials. PrePullTime plus holdTime must be < recordingTime for un-cued trials
+    """
+    defaultToGoalTime = .25 # mouse has this many seconds from start cue (cued trials only)to get lever into goal area, or trial is ended.
+    defaultHoldTime = 0.2 # mouse must hold lever in goal area for this long for success, can be overridden by training settings
+    defaultGoalCenter = 200 # center of goal area where mouse must maintain lever for success
+    defaultGoalWidth = 100 # width of goal area where mouse must maintain lever for success, can be overridden by training settings
+    """
+    Perturbation settings for starting a mouse. Force on lever is adjusted up or down relative to const force, and then left at new value, and lever must be kept in goal area 
+    """
+    defaultPerturbPercent = 0.0 # 0 means no perturbation, just hold lever in goal area for hold time, non-zero value (0-1) means proportion of trials to perturb
+    defaultPerturbRampDur = 0.25 # duration of ramp for perturbationb, in seconds, max possible duration is set by MAX_FORCE_ARRAY_SIZE / LEVER_FREQ
+    defaultPerturbStartTime # time from ToGoalTime to start of perturbation
+    defaultPerturbStartRandom = 0 # 0 randomized time in seconds plus/minus applied to perturbStart time, 0 means do not randomize perturb start time
+    defaultPerturbForceOffset = 0 # added force on lever during a perturbation relative to constant force. If perturbForceOffset is 0, randomized force is symetric around constantForce
+    defaultPerturbForceRandom = 400 # randomized force range plus/minus applied to constantForce + perturb force, 0 means do not randomize
+    """
+    Training settings
+    """
+    defaultTrainSize = 50 # number of previous trials to examine when deciding promotion/demotion
+    defaultPromote = 0.75 # promote to next level when this performance rate is exceeded
+    defaultFail = 0.25 # drop to previous level when fail rate is not met
+    """
+    training for goal position
+    """
+    defaultGoalTrainOn = 0 # bit 0 means training, bit 1 means demotion is not allowed, only promotion to next level
+    defaultGoalStartWidth = 100
+    defaultGoalEndWidth = 10
+    defaultGoalIncr = 5
+     """
+    training for holdTime
+    """
+    defaultHoldTrainOn = False
+    defaultHoldStartTime = 0.1
+    defaultHoldEndTime = 2
+    defaultHoldIncr = 0.1
+    
+    #pathToData_def = '/home/pi/Documents/' # folder where lever pos files will be saved, or maybe just pass the array to the DataLogger
     
     @staticmethod
     def about():
