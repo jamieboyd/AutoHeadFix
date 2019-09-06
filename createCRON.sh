1e174e4b5d12ff5d09e2574f8b5cc03bffc50e35
Judge24601 <braedenjury@gmail.com>
2019-09-06 15:37:25 -0700

    cron working

--- createCRON.sh
+++ createCRON.sh
@@ -3,12 +3,15 @@
 #******************************************************************
 # This script searches for auto_start_script, if found will create a cronjob as root that opens auto start script every hour
 #*******************************************************************
-mypath=$(sudo find / -type f -name "auto_start_script.sh" | grep "auto_start_script.sh";)
+mypath=$(sudo find / -type d -name "AutoHeadFix" | grep "AutoHeadFix";)
 
 if [ $? -eq 0 ];
 	        then
-			(echo "0 * * * * /bin/bash $mypath") > croninfo.txt
-			sudo crontab -l -u root | cat - croninfo.txt | sudo crontab -u root -
+			sudo crontab -l > croninfo.txt
+			(echo "0 * * * * /bin/bash $mypath/auto_start_script.sh > $mypath/auto_start_error.txt 2>&1") >> croninfo.txt
+			(echo "30 * * * * /usr/bin/python3 $mypath/water_failsafe.py > $mypath/water_failsafe_error.txt 2>&1") >> croninfo.txt
+			cat croninfo.txt | sudo crontab -u root -
+			rm -rf croninfo.txt
 		else 
 
 			echo "unable to generate cronjob"
