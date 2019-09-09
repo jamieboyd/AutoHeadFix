9b108524fba450bb5a2b62c37e03020d97d0488f
Judge24601 <braedenjury@gmail.com>
2019-09-09 16:37:01 -0700

    Text notifier added

--- auto_start_script.sh
+++ auto_start_script.sh
@@ -5,5 +5,6 @@ export DISPLAY
 if [[ ! $(pgrep -f __main__2.py ) ]]; then
 	echo "start script" > /home/pi/tmp.txt
 	exec /usr/bin/python3 -u /home/pi/Desktop/AHF_setup/AutoHeadFixSetup/AutoHeadFix/__main__2.py --temp >>/home/pi/Desktop/log.txt 2>&1
-	exec /usr/bin/python3 /home/pi/Desktop/AHF_setup/AutoHeadFixSetup/AutoHeadFix/text_Notifier.py &> /home/pi/Desktop/notifier_Log.txt
+	exec /usr/bin/python3 /home/pi/Sms/Send_text.py > /home/pi/Desktop/notifier_Log.txt 2>&1
+	
 fi
