ce3efd650625d0468282f2503ee17a603d202bd8
zenenhornstein <zenenhornstein2@gmail.com>
2019-09-12 12:05:58 -0700

    adding cage ID as command line option:

--- auto_start_script.sh
+++ auto_start_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DISPLAY=":0.0"
 export DISPLAY
+mypath=$(sudo find / -type d -name "AutoHeadFix" | grep "AutoHeadFix";)
 
 if [[ ! $(pgrep -f __main__2.py ) ]]; then
 	echo "start script" > /home/pi/tmp.txt
-	exec /usr/bin/python3 -u /home/pi/Desktop/AHF_setup/AutoHeadFixSetup/AutoHeadFix/__main__2.py --temp >>/home/pi/Desktop/log.txt 2>&1
+	exec /usr/bin/python3 -u $mypath/__main__2.py --temp $($mypath/load_Config.sh cageid) >>/home/pi/Desktop/log.txt 2>&1
 	exec /usr/bin/python3 /home/pi/Sms/Send_text.py > /home/pi/Desktop/notifier_Log.txt 2>&1
 	
 fi
