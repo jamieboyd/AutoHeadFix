e40e4df4224999fb9487943faebbc5a3b2e08915
zenenhornstein <zenenhornstein2@gmail.com>
2019-10-10 11:53:02 -0700

    cleaned up configuration fetching

--- auto_start_script.sh
+++ auto_start_script.sh
@@ -2,11 +2,14 @@
 #DISPLAY=":0.0"
 #export DISPLAY
 #mypath=$(sudo find / -type d -name "AutoHeadFix" | grep "AutoHeadFix";)
-mypath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mainpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+configpath="/home/pi"
+
 
 if [[ ! $(pgrep -f __main__2.py ) ]]; then
 	echo "start script" > /home/pi/tmp.txt
-	exec /usr/bin/python3 -u $mypath/__main__2.py --temp $($mypath/load_Config.sh cageid) >>/home/pi/Desktop/log.txt 2>&1
+	exec /usr/bin/python3 -u $mainpath/__main__2.py --temp $($mypath/load_Config.sh cageid) >>/home/pi/Desktop/log.txt 2>&1
 	exec /usr/bin/python3 /home/pi/Sms/Send_text.py > /home/pi/Desktop/notifier_Log.txt 2>&1
 	
 fi
--- load_Config.sh
+++ load_Config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #This program takes in a command line argument as the element you wish to extract from config.txt
 # Returns everything to the right (not including) the equal sign
-mypath=$(sudo find / -type d -name "AutoHeadFix" 2>&1 |grep -v "Permission denied" | grep "AutoHeadFix";)
+#mypath=$(sudo find / -type d -name "AutoHeadFix" 2>&1 |grep -v "Permission denied" | grep "AutoHeadFix";)
+DIR="/home/pi"
 
-cat $mypath/config.txt | grep -i -e $1 | grep -o -e =.* | grep -o -e [^=].*
+cat $DIR/config.txt | grep -i -e $1 | grep -o -e =.* | grep -o -e [^=].*
